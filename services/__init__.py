3184618a3a4d793d9a06523a725e2100b48737a0
Mraprguild8133 <mraprguild06@gmail.com>
2025-08-11 13:37:45 +0530

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -6,4 +6,4 @@ from .base_ai_service import BaseAIService
 from .gemini_service import GeminiService
 from .together_service import TogetherService
 
-__all__ = ['BaseAIService', 'OpenAIService', 'GeminiService', 'TogetherService']
+__all__ = ['BaseAIService',  'GeminiService', 'TogetherService']
