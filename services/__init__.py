273b3a1713e9a03c43825ab4b1a48032d863bd9c
Mraprguild8133 <mraprguild06@gmail.com>
2025-08-11 13:37:26 +0530

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -3,7 +3,6 @@ AI Services package
 """
 
 from .base_ai_service import BaseAIService
-from .openai_service import OpenAIService
 from .gemini_service import GeminiService
 from .together_service import TogetherService
 
